d35b26f7c1f9eb90b2971ddaf0222710698a2ea7
taicrx <taicrx@gmail.com>
2026-06-10 16:55:54 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ Use /pub?...&output=csv URLs, not normal /edit URLs.
 
 const ASP_CONFIG = {
   FACT_ASP_MONTHLY_CSV_URL:
-    "REPLACE_WITH_FACT_ASP_MONTHLY_CSV_URL",
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQM8WMnPop68JT32GelxEr2Nf77TcQKXAZvATTlsj59kKxzQFIfkRzXB7UwgpE5pXYElaWIDkDyYHsZ/pub?gid=257837179&single=true&output=csv",
 
   FACT_ANTIBIOGRAM_CSV_URL:
-    "REPLACE_WITH_FACT_ANTIBIOGRAM_MASTER_CSV_URL"
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQM8WMnPop68JT32GelxEr2Nf77TcQKXAZvATTlsj59kKxzQFIfkRzXB7UwgpE5pXYElaWIDkDyYHsZ/pub?gid=548638764&single=true&output=csv"
 };
